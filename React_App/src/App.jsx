e9404719fd151a2d10b9275741750b63d7e38cd0
ma000509 <rodriguezboue@gmail.com>
2022-08-16 00:19:53 -0400

    Ahora si esta funcional:)

--- React_App/src/App.jsx
+++ React_App/src/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import styles from "./style";
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import { Billing, Business, CTA, CardDeal, Clients, Footer, Hero, Navbar, Stats, Testimonials } from "./componets";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
- 
+  
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
@@ -14,8 +15,8 @@ const App = () => (
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
-    </div>
-    
+    </div>   
+
     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
---
+++ React_App/src/componets/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import{ close, logo, menu } from "../assets"
+import { navLinks } from "../constants"
+const NavBar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar" >
+        <img src={logo} alt = "HookBank" className= "w-[128px] h-[32px] "/>
+        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+            {navLinks.map((nav, index) =>(
+               <li key = {nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length-1 ? 'mr-0' : 'mr-10'}`}>
+                    <a href={`#${nav.id}`}>
+                        {nav.title}
+                    </a>
+               </li> 
+            ))}
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
